b44d91698783503f195bff87b773deaefbda615f
PlatinumPlace <warlyn200@gmail.com>
2020-01-02 13:10:41 -0400

    Preboot

--- core/views/home/alerta.php
+++
@@ -1,6 +0,0 @@
-<div class="alert alert-<?=$alerta?>" role="alert">
-  <h4 class="alert-heading"><?=$titulo?></h4>
-  <p><?=$descripcion?></p>
-  <hr>
-  <a href="<?=$accion?>" class="btn btn-info">Aceptar</a>
-</div>
